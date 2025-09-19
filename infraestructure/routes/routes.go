853c6d5a5750dbe42fd79bde2a9f3d3e0a438eaf
GeordyM <ggeordymm@gmail.com>
2025-09-18 22:10:04 -0500

    Añadidos cambios

--- infraestructure/routes/routes.go
+++ infraestructure/routes/routes.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	"github.com/geordym/pendientico/application/handlers"
-	"github.com/geordym/pendientico/middlewares"
 	"github.com/labstack/echo/v4"
 )
 
 func Init(e *echo.Echo) {
-	e.GET("/users", handlers.GetUsers, middlewares.AuthMiddleware)
+	//e.GET("/users", handlers.GetUsers, middlewares.AuthMiddleware)
+	e.GET("/users", handlers.GetUsers)
+
 }
